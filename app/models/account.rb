87e88b242a15247722a24a3515ba424ce382bc18
Thomas Troelsen <ttro@ucsyd.dk>
2014-05-23 11:55:23 +0200

    11 -> Nu virker login+create

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,20 +3,4 @@ class Account < ActiveRecord::Base
   has_many :user_accounts
   has_many :users, through: :user_accounts
 
-  def self.constructor
-    where('account_id = ?', 9)
-  end
-
-  def self.administrator
-    where('account_id >= ?', 3)
-  end
-
-  def self.editor
-    where('account_id >= ?', 2)
-  end
-
-  def self.contributor
-    where('account_id >= ?', 1)
-  end
-
 end
