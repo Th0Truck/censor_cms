751c8ad7d64e70d471b04d6b3831880cff744f9f
Thomas Troelsen <ttro@ucsyd.dk>
2014-06-17 10:41:09 +0200

    14 -> Så kører multiple-user, cross domain login.......... Yæi!

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -70,6 +70,6 @@ class SettingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def setting_params
-      params.require(:setting).permit(:domain, :name, :login, :footer, :info, :analytics_api, :facebook, :googleplus, :linkedin)
+      params.require(:setting).permit(:domain, :name, :login, :footer, :info, :analytics_api, :facebook, :googleplus, :linkedin, :css, :js)
     end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,7 +5,7 @@ class Setting < ActiveRecord::Base
   has_many :sidebars
   has_many :users, :through => :user_settings
 
-  attr_accessible :domain, :name, :login, :footer, :info, :analytics_api, :facebook, :facebook_secret, :googleplus, :googleplus_secret, :linkedin, :linkedin_secret, :homepage
+  attr_accessible :domain, :name, :login, :footer, :info, :analytics_api, :facebook, :facebook_secret, :googleplus, :googleplus_secret, :linkedin, :linkedin_secret, :homepage, :css, :js
 
   def self.for_host(host)
     subdomain = host.split('.').first
