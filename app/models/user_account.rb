8bc85c51bb1d8530a739f95d1fb4ab7bc39fccf4
Thomas Troelsen <ttro@ucsyd.dk>
2014-08-12 11:26:19 +0200

    16 -> Fix til session.create?

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -3,5 +3,5 @@ class UserAccount < ActiveRecord::Base
   belongs_to :user
   belongs_to :account
 
-  attr_accessible :password_hash, :password_salt
+  attr_accessible :password_hash, :password_salt, :password
 end
